018a1637b217896bc27733362e16f8f2de26dea2
DavCode1 <davidmnr571@gmail.com>
2025-06-16 14:20:38 +0200

    modif chemin de fichier pour avoir fichier frontend

---
+++ Frontend-cookneat/public/server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connecter MongoDB Atlas
+const URI = 'mongodb+srv://davidmnr571:fMnGnvu5clhYJBbm@cookneatcluster.t55e9.mongodb.net/?retryWrites=true&w=majority&appName=CookNeatClusternp';
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+// Définir un modèle MongoDB
+const ItemSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+});
+const Item = mongoose.model('Item', ItemSchema);
+
+// Routes
+app.get('/items', async (req, res) => {
+  const items = await Item.find();
+  res.json(items);
+});
+
+app.post('/items', async (req, res) => {
+  const newItem = new Item(req.body);
+  await newItem.save();
+  res.json(newItem);
+});
+
+// Démarrer le serveur
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
