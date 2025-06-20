e0e2ee2e32c25fa52301586b6bc65533e8fd8dac
DavCode1 <davidmnr571@gmail.com>
2025-06-20 19:47:35 +0200

    e

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ app.use('/uploads', express.static('uploads'));
 app.get('/', (req, res) => {
   res.send('✅ API CookNeat opérationnelle');
 });
-
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 // 📡 Connexion MongoDB
 const mongoUri = process.env.MONGO_URI;
 if (!mongoUri) {
