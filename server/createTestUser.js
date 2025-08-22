68d0283561a8aafb314fc0a50c3bf07af0765e45
DavCode1 <davidmnr571@gmail.com>
2025-08-22 19:47:23 +0200

    d

--- server/createTestUser.js
+++ server/createTestUser.js
@@ -1,35 +1,13 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const User = require("./models/User");
-require('dotenv').config();
 
-const MONGO_URI = process.env.MONGO_URI || "<mongodb+srv://cookadmin:cookneat123@cookneat-db.anbbadf.mongodb.net/cookneat?retryWrites=true&w=majority&appName=cookneat-db>";
-
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connexion à MongoDB réussie"))
-  .catch((err) => console.error("Erreur de connexion MongoDB :", err));
+mongoose.connect("<URL>", { useNewUrlParser: true, useUnifiedTopology: true });
 
 (async () => {
-  try {
-    const hashedPassword = await bcrypt.hash("123456", 10);
-
-    const existingUser = await User.findOne({ email: "test@test.com" });
-    if (existingUser) {
-      console.log("L'utilisateur test existe déjà !");
-      process.exit();
-    }
-
-    const newUser = await User.create({
-      username: "testuser",
-      email: "test@test.com",
-      password: hashedPassword,
-      role: "user"
-    });
-
-    console.log("Utilisateur test créé :", newUser);
-    process.exit();
-  } catch (err) {
-    console.error("Erreur lors de la création de l'utilisateur test :", err);
-    process.exit(1);
-  }
+  await User.deleteOne({ email: "test@test.com" });
+  const hashedPassword = await bcrypt.hash("123456", 10);
+  await User.create({ username: "testuser", email: "test@test.com", password: hashedPassword, role: "user" });
+  console.log("User recréé !");
+  process.exit();
 })();
