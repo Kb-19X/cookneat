516f435daa0f9feef377c605a4fb5926c2948e11
DavCode1 <davidmnr571@gmail.com>
2025-08-26 23:48:14 +0200

    ajout path

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const path = require('path');
 const authRoutes = require('./routes/auth');
 const recipeRoutes = require('./routes/recipes');
 const commentRoutes = require('./routes/comments');
