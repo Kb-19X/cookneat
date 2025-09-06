c592130066b063d0e1c1efdafba94ce386a16bd5
DavCode1 <davidmnr571@gmail.com>
2025-09-06 17:22:00 +0200

    debug

--- server/models/User.js
+++ server/models/User.js
@@ -8,11 +8,12 @@ const userSchema = new mongoose.Schema({
   role:     { type: String, enum: ["user", "admin"], default: "user" }
 }, { timestamps: true });
 
+/**
 userSchema.pre("save", async function(next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
-});
+}); */
 
 userSchema.methods.comparePassword = function(password) {
   return bcrypt.compare(password, this.password);
